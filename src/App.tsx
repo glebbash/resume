18a59e337e64b21670811b4f9c92f03e6e859f06
glebbash <glebbash@gmail.com>
2020-11-12 13:08:58 +0100

    Add preffered theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
 import React from "react"
-import { createStyles, makeStyles } from "@material-ui/core/styles"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
-import NamedIcon from "./components/NamedIcon"
-import { Stars, Work, School, Code, Info } from "@material-ui/icons"
-import { Grid } from "@material-ui/core"
-
-const useStyles = makeStyles(() =>
-    createStyles({
-        root: {},
-    }),
-)
+import MainPage from "./pages/MainPage"
 
 export default function App() {
-    const cls = useStyles()
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
+
+    const theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                palette: {
+                    type: prefersDarkMode ? "dark" : "light",
+                },
+            }),
+        [prefersDarkMode],
+    )
 
     return (
-        <Grid className={cls.root}>
+        <ThemeProvider theme={theme}>
             <CssBaseline />
             {/* Roboto font */}
             <link
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
             />
-            <Grid container direction="column">
-                <NamedIcon icon={Stars}>Key skills</NamedIcon>
-                <NamedIcon icon={Work}>Work history</NamedIcon>
-                <NamedIcon icon={School}>Education</NamedIcon>
-                <NamedIcon icon={Code}>Projects</NamedIcon>
-                <NamedIcon icon={Info}>Additional info</NamedIcon>
-            </Grid>
-        </Grid>
+            <MainPage />
+        </ThemeProvider>
     )
 }
